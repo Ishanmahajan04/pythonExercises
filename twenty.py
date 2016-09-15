598fc221b9812e0821c1edd5747c9a240d5335c4
Ishanmahajan04 <ishan04mahajan@gmail.com>
2016-09-15 12:10:06 +0530

    Fourth commit

---
+++ twenty.py
@@ -0,0 +1,8 @@
+dictionary = {"merry":"god", "christmas":"jul", "and":"och",
+"happy":"gott", "new":"nytt", "year":"ar"}
+
+def translate(lst):
+  return [dictionary[w.lower()] for w in lst if w.lower() in dictionary]
+
+
+print (translate(['Merry', 'christmas', 'and', 'happy', 'new', 'year', 'mom']))
